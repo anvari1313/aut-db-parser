94ab471d4d84f17032e3fc051964868b50012149
Ahmad Anvari <ahmad.anv@gmail.com>
2017-11-07 21:25:40 +0330

    Adding passport local strategy authentication and prefix route for faculty

--- app.js
+++ app.js
@@ -5,12 +5,33 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const session = require('express-session')
+const flash = require('connect-flash');
+
+// var passport = require('passport')
+//     , LocalStrategy = require('passport-local').Strategy;
+
+const passport = require('./middleware/authentication').passport;
+const connectEnsureLogin = require('./middleware/authentication').connectEnsureLogin;
 const index = require('./routes/index');
-const users = require('./routes/users');
+const home = require('./routes/home');
 const test = require('./routes/test');
 const post = require('./routes/post');
+const faculty = require('./routes/faculty');
 
 let app = express();
+
+
+app.use(express.static("public"));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,13 +46,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/', home);
 app.use('/test', test);
-app.use('/post', post);
+app.use('/post', connectEnsureLogin.ensureLoggedIn(), post);
+app.use('/faculty',
+    // connectEnsureLogin.ensureLoggedIn(),
+    faculty);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
