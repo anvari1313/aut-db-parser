a0e021a09f912f4c82a148fa4b0a295cc94f050d
Ahmad Anvari <ahmad.anv@gmail.com>
2017-11-13 16:37:55 +0330

    Adding route for porting a single faculty

--- routes/faculty.js
+++ routes/faculty.js
@@ -3,5 +3,7 @@ let router = express.Router();
 const FacultyController = require('./../controller/provider/faculty');
 
 router.get('/:faculty_id', FacultyController.getSingle);
+router.get('/:faculty_id/port', FacultyController.portSingle);
+
 
 module.exports = router;
