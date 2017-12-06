871f3115f242d1bfbebc77f423234343400c30d8
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-06 12:14:15 +0330

    Bug fix for dept

--- routes/dept.js
+++ routes/dept.js
@@ -6,7 +6,7 @@ const DeptController = require('./../controller/provider/dept');
 const FacultyController = require('./../controller/provider/faculty');
 
 router.get('/', DeptController.getAll);
-router.get('/:dept_id', );
+router.get('/:dept_id', FacultyController.getOfDept);
 // router.get('/:faculty_id', connectEnsureLogin.ensureLoggedIn(), FacultyController.getSingle);
 // router.get('/:faculty_id/port', connectEnsureLogin.ensureLoggedIn(), FacultyController.portSingle);
 // router.get('/:faculty_id/port.json', jwt_extractor, FacultyController.portSingleJson);
