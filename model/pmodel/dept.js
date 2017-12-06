2924f260ad041f9b34677fe3fa7f0ce4415316d2
Ahmad Anvari <ahmad.anv@gmail.com>
2017-12-06 11:03:12 +0330

    Adding sync requests

--- model/pmodel/dept.js
+++ model/pmodel/dept.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const Post = require('./../cmodel/post');
 const PostMeta = require('./../cmodel/post-meta');
 const moment = require('moment');
+const Promise = require('bluebird');
+const request = require('sync-request');
 
 class Dept extends ProviderModel{
     constructor(modelObj)
@@ -28,11 +30,31 @@ class Dept extends ProviderModel{
     }
 
     static getAll(){
-        console.log(this.providingListPath);
-        return axios.get(this.providingListPath).
-        then(response => {
-            return Promise.resolve(response);
+
+        let array = [];
+
+        return new Promise((resolve, reject) => {
+            let currentRequestingLink = this.providingListPath;
+            do {
+                // console.log('This is here');
+                let res = request('GET', currentRequestingLink);
+                let jsonBody = JSON.parse(res.body.toString());
+                console.log(jsonBody.items);
+                array.concat(jsonBody.items);
+                if (jsonBody.next)
+                    currentRequestingLink = jsonBody.next["$ref"];
+                else
+                    currentRequestingLink = null;
+
+            }while (currentRequestingLink);
+            
+            resolve(array);
         });
+
+        // return axios.get(this.providingListPath).
+        // then(response => {
+        //     return Promise.resolve(response.data);
+        // });
     }
 
 }
